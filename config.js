66134fc104aa4b74eb8c3194ec6e0fdb2408d1df
Kinglakiya <138453863+Kinglakiya@users.noreply.github.com>
2023-07-04 16:33:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94771894708', 'King Lakiya', true], 
   [''] 
 ] //Number of owners
 
